043c8570a4c7514f9f004f9ffaccedb3aa5de279
chart21 <chart21@web.de>
2024-05-22 14:43:28 +0200

    fixed benchmark scripts

--- files/samples/conv_runner.sh
+++ files/samples/conv_runner.sh
@@ -6,9 +6,13 @@ b=$2
 #get n from command line argument
 n=$3
 
-if [ $b -eq 32 && $n -eq 3 ]; then
-    CUDA_VISIBLE_DEVICES=0 /bin/time -v ./piranha32_3 -p $1 -c files/samples/benchmark_conv.json elif [ $b -eq 64 && $n -eq 3]; then
-    CUDA_VISIBLE_DEVICES=0 /bin/time -v ./piranha64_3 -p $1 -c files/samples/benchmark_conv.json elif [ $b -eq 32 && $n -eq 4]; then
-    CUDA_VISIBLE_DEVICES=0 /bin/time -v ./piranha32_4 -p $1 -c files/samples/benchmark_conv.json elif [ $b -eq 64 && $n -eq 4]; then
-    CUDA_VISIBLE_DEVICES=0 /bin/time -v ./piranha64_4 -p $1 -c files/samples/benchmark_conv.json
+if [ $b -eq 32 ] && [ $n -eq 3 ]; then
+    CUDA_VISIBLE_DEVICES=0 ./piranha32_3 -p $p -c files/samples/benchmark_conv.json
+elif [ $b -eq 64 ] && [ $n -eq 3 ]; then
+    CUDA_VISIBLE_DEVICES=0 ./piranha64_3 -p $p -c files/samples/benchmark_conv.json
+elif [ $b -eq 32 ] && [ $n -eq 4 ]; then
+    CUDA_VISIBLE_DEVICES=0 ./piranha32_4 -p $p -c files/samples/benchmark_conv.json
+elif [ $b -eq 64 ] && [ $n -eq 4 ]; then
+    CUDA_VISIBLE_DEVICES=0 ./piranha64_4 -p $p -c files/samples/benchmark_conv.json
 fi
+
